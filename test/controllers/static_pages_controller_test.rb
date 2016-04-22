1ea7c806beefcf2a0ed22786866e1922c60dd4c5
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-22 14:08:28 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | My first Ruby on Rails application"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | My first Ruby on Rails application"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | My first Ruby on Rails application"
+  end
 end
