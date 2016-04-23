d1812863c5f2d1fa0fda97e3ba5b59483e609010
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-23 15:21:11 -0400

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | My first Ruby on Rails application"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | My first Ruby on Rails application"
+  end
 end
