7134a437a52d3afbdea8a3d7e1f224ef584199e5
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-25 21:54:01 -0400

    Finish user signup and add tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      flash[:success] = "Welcome to my first Ruby on Rails App!"
+      redirect_to @user
     else
       render 'new'
     end
