cf6ae5010a5884d9e29b1845ab4da0046e896bd3
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-26 12:06:05 -0400

    Implement layout changes for loggedin user vs. logged out user. Add integration tests for user login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -6,9 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page
+      log_in user
+      redirect_to user
     else
       # Create an error message
-      flash.now[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
