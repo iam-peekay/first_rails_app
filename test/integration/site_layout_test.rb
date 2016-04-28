71760b618304acc312f75b297af721d8d614a63a
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-28 14:52:22 -0700

    Add tests for ensuring admin attribute cannot be modified. Refactor new & edit html using partials

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,4 +11,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  test "help path" do
+    get help_path
+    assert_template 'static_pages/help'
+    assert_select "title", full_title("Help")
+  end
+
+  test "about path" do
+    get about_path
+    assert_template 'static_pages/about'
+    assert_select "title", full_title("About")
+  end
+
+  test "contact path" do
+    get contact_path
+    assert_template 'static_pages/contact'
+    assert_select "title", full_title("Contact")
+  end
 end
