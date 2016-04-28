cb00530ffc701e95fa34dafae1ad9b74bcb91909
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-27 19:29:28 -0700

    Add functionality to forward user to intended page if not logged in and then logs in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,7 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+   # Remembers a user in a persistent session.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
@@ -27,6 +28,11 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   # Forgets a persistent session.
   def forget(user)
     user.forget
@@ -39,5 +45,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
