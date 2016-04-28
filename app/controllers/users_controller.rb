8f9da8b60ef2f62769d681d3b784d2a9892fee60
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-27 19:39:50 -0700

    Add page for user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
 
+  def index
+    @user = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
   end
@@ -33,6 +37,7 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
   private
 
     def user_params
