7bbc8fe225cacec0029d136db439ca9b775e153f
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-04-28 19:24:38 -0700

    Complete account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,17 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
