32fbee3b4533c58a29f8501d52959834fa4ef220
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-05-01 12:06:01 -0700

    Add microposts feed to a users profile page

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,6 +68,12 @@ class User < ActiveRecord::Base
     reset_sent_at < 2.hours.ago
   end
 
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
     # Converts email to all lower-case.
     def downcase_email
