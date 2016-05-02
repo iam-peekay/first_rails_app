b415510e2ccb8fc8e96969604726cf50d14285a2
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-05-02 11:22:55 -0700

    add follower and following buttons and pages to user profile. Add integration tests for relationships

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -67,4 +67,14 @@ class UsersControllerTest < ActionController::TestCase
                                     }
     assert_not @other_user.admin?
   end
+
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
 end
